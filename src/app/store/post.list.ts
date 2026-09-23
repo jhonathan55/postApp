a9aef6be626aae918a53dec2957eb1e317695b11
jhonathan55 <jfgutierrezsaico@gmail.com>
2026-09-22 21:05:24 -0300

    script json

--- src/app/store/post.list.ts
+++ src/app/store/post.list.ts
@@ -1,13 +1,6 @@
 import { createSelector, createFeatureSelector } from '@ngrx/store';
 import { PostState } from '../models/post-state.models';
 
-// export const selectPostState = createFeatureSelector<PostState>('posts');
-
-// export const selectPosts = createSelector(
-//   selectPostState,
-//   (state: PostState) => state.posts
-// );
-
 export const selectPostState = createFeatureSelector<PostState>('posts');
 
 export const selectFilteredPosts = createSelector(
